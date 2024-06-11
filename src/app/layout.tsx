688067db6fd2ce05e616431be2794bfba74334d1
Henrique Lopes <h.lopes.silva2015@gmail.com>
2024-06-11 01:59:30 -0300

    Feat: Atualização do boilerplate

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
+
+import '@styles/globals.css'
+
+const roboto = Roboto({ subsets: ['latin'], weight: '500' })
+
+export const metadata: Metadata = {
+  title: 'Layout NextJS',
+  description: 'Um layout de next'
+}
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <section className="h-full flex justify-center items-center">
+      <h1 className="text-3xl">Boilerplate de Nextjs</h1>
+    </section>
+  )
+}
